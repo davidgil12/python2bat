8b2d181df7313f9069780cc8fb14ace1bebc5c9d
davidgil12 <al.davidgil@iesramonllull.net>
2024-10-11 13:07:27 +0200

    second commit

--- prueba.py
+++ prueba.py
@@ -3,3 +3,13 @@ print("Hola Marina")
 #  LA FUNCION INPUT PERMITE INTRODUCIR INFORMACION POR TECLADO
 input()
 #  
+
+def(t,n,r,p):
+    A=p*(1+r/n)**(n*t)
+    return A
+p= float(input("Capital Inicial"))
+r= float(input("Interes anual en %"))
+t= float(input("numero de años"))
+n= float(input("numero de periodo por años"))
+
+print(f("La cantidad final es el resultado de {t} y {n}"))
