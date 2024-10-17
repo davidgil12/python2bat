dffe552eedce3bede39a62a59dc688f6802f9810
davidgil12 <al.davidgil@iesramonllull.net>
2024-10-17 13:07:23 +0200

    4 commit

---
+++ conjuntos.py
@@ -0,0 +1,14 @@
+conjunto1 = {1,2,3,4}
+conjunto2 = {2,4,6}
+
+conjunto1.add(5)
+print(conjunto1)
+
+union = conjunto1 | conjunto2
+print(union)
+
+interseccion = conjunto1 & conjunto2 
+print(interseccion)
+
+diferencia = conjunto1 - conjunto2 
+print(diferencia)
---
+++ tuplas.py
@@ -0,0 +1,8 @@
+coordenadas = (34.05, -118.24)
+size = ("S", "M", "L", "XL", "XXL")
+
+print(coordenadas[1])
+print(coordenadas)
+
+print(size)
+print(size[2])
